46179b2c5500d32a2709dafd316f01e0f77d15b4
OmarAliSoliman <49602133+OmarAliSoliman@users.noreply.github.com>
2019-09-16 00:41:45 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-   
-  });
+const input = document.querySelectorAll('form input');
+const label = document.querySelectorAll('form label');
+const icon = document.querySelectorAll('form label i');
+
+function make(index){
+  label[index].style.fontSize="10px";
+  icon[index].style.color = "#AD1457";
+}
+
+function dele(index){
+  label[index].style.fontSize="15px";
+  icon[index].style.color = "#AFAFAF";
+}
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+    
